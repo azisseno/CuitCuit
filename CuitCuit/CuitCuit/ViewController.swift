0b2f55e9052f14b3adbfabc9b246c07090df2497
Gelar Aulia Prima Putra <prima.gelar@gmail.com>
2020-02-28 15:03:39 +0700

    add initial files

---
+++ CuitCuit/CuitCuit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CuitCuit
+//
+//  Created by Gelar Aulia Prima Putra on 28/02/20.
+//  Copyright © 2020 Gelar Aulia Prima Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
